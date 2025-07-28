516c58cfaf23d851e3bd15d350c712090fa247da
adrlorart <adri.lore@hotmail.com>
2025-07-28 17:06:14 -0600

    funcion agregar amigo junto con funcion de limpiar cuadro de texto

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+//VARIABALES
+let listaAmigos= [];
+
+
+// |FUNCIONES|
+//guardar en una lista
+function agregarAmigo(){
+    if ((document.getElementById("amigo").value) == ''){
+        alert("Por favor, inserte un nombre");
+    }else{
+        listaAmigos.push(document.getElementById("amigo").value);
+        console.log(`Elementos de la lista: ${listaAmigos}`);
+        limpiar();
+    }
+}
+//limpiar cuadro de texto
+function limpiar(){
+    document.querySelector('#amigo').value = '';
+}
+//imprimir lista en pantalla id=listaAmigos
+function listaEnPantalla(){
+    
+}
